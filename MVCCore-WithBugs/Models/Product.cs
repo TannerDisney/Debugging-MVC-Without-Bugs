47ac8ce8f4a4759315125a61f3f9ddcebc86b8fe
Tanner Disney <disneytanner@gmail.com>
2019-08-14 12:47:33 -0700

    Add project files.

---
+++ MVCCore-WithBugs/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCore_WithBugs.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Product Price")]
+        public double ProductPrice { get; set; }
+
+
+    }
+}
---
+++ MVCCore-WithBugs/Models/ProductDb.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCore_WithBugs.Models
+{
+    public static class ProductDb
+    {
+        public static List<Product> GetAllProducts(ProductContext _context)
+        {
+            List<Product> products =
+                (from prod in _context.Product
+                 orderby prod.ProductName ascending
+                 select prod).ToList();
+            return products;
+        }
+
+        public static Product AddProduct(ProductContext context, Product prod)
+        {
+            context.Add(prod);
+            context.SaveChanges();
+            return prod;
+        }
+    }
+}
